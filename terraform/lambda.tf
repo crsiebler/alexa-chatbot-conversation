053abff770b500fd776292027001afaad8713b24
Cory Siebler <cory.siebler@protonmail.com>
2025-12-20 17:46:38 -0700

    style: format Terraform files (#16)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -40,12 +40,12 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "alexa_chatbot" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = var.function_name
-  role            = aws_iam_role.lambda_execution_role.arn
-  handler         = "index.handler"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime         = var.lambda_runtime
-  timeout         = var.lambda_timeout
-  memory_size     = var.lambda_memory_size
+  runtime          = var.lambda_runtime
+  timeout          = var.lambda_timeout
+  memory_size      = var.lambda_memory_size
 
   environment {
     variables = {
